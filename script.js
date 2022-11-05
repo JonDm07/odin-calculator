44d9de54c28758ccb07dabd1622899e0206292ed
JonDm07 <jan.drmota1@gmail.com>
2022-11-05 14:39:46 +0100

    Make buttons work
    All buttons except Clear now mostly work

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let firstNumber;
+let secondNumber;
+let operation;
+let result;
+
 function add(a, b) {
     return a + b
 }
@@ -17,6 +22,7 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
     if (operator === "+") {
         return add(num1, num2)
+        result = add
 
     } else if (operator === "-") {
         return subtract(num1, num2)
@@ -30,5 +36,53 @@ function operate(operator, num1, num2) {
     }
 }
 
-console.log(operate("/", 3, 3))
+const display = document.querySelector(".display")
+
+function showResult(func) {
+    display.textContent = func
+
+}
+
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        operation = operator.textContent
+        console.log(operation)
+    })
+})
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if(firstNumber === undefined) {
+            firstNumber = number.textContent
+            console.log(`1 = ${firstNumber}`)
+        } else if (firstNumber !== undefined) {
+            secondNumber = number.textContent
+            console.log(`2 = ${secondNumber}`)
+        } else {
+            console.log("error")
+        }
+    })
+
+})
+
+const equalSign = document.querySelector(".equalSign")
+
+equalSign.addEventListener("click", () => {
+    
+    firstNumber = parseFloat(firstNumber)
+    secondNumber = parseFloat(secondNumber)
+    showResult(operate(operation, firstNumber, secondNumber))
+    result = operate(operation, firstNumber, secondNumber)
+    firstNumber = undefined
+    secondNumber = undefined
+    console.log(firstNumber)
+    console.log(secondNumber)
+
+})
+
+
+
 
