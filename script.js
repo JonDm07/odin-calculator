a635f13f7e80bd67289223b2af92ed71833b826c
JonDm07 <jan.drmota1@gmail.com>
2022-11-13 20:55:00 +0100

    Fix bugs and add 0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNumber;
-let secondNumber;
-let operation;
+let firstNumber = ""
+let secondNumber = ""
+let operation = ""
 let result;
 
 function add(a, b) {
@@ -22,7 +22,7 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
     if (operator === "+") {
         return add(num1, num2)
-        result = add
+
 
     } else if (operator === "-") {
         return subtract(num1, num2)
@@ -48,41 +48,93 @@ const operators = document.querySelectorAll(".operator")
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
-        operation = operator.textContent
-        console.log(operation)
+        if (operation === "") {
+            operation += operator.textContent
+            console.log(operation)
+        } else if (firstNumber !== "" && secondNumber !== "" && operation !== "" ) {
+            firstNumber = parseFloat(firstNumber)
+            secondNumber = parseFloat(secondNumber)
+            result = operate(operation, firstNumber, secondNumber)
+            operation = ""
+            operation += operator.textContent
+            firstNumber = ""
+            secondNumber = ""
+
+        } else if (result !== "" && secondNumber !== "" && operation !== "") {
+            secondNumber = parseFloat(secondNumber)
+            result = operate(operation, result, secondNumber)
+            operation = ""
+            operation += operator.textContent
+            secondNumber = ""
+
+        }
+
+        if (result !== undefined) {
+            display.textContent = `${result} ${operation} ${secondNumber}`
+        } else {
+            display.textContent = `${firstNumber} ${operation} ${secondNumber}`
+        }
     })
 })
 
 numbers.forEach((number) => {
-    number.addEventListener("click", () => {
-        if(firstNumber === undefined) {
-            firstNumber = number.textContent
-            console.log(`1 = ${firstNumber}`)
-        } else if (firstNumber !== undefined) {
-            secondNumber = number.textContent
-            console.log(`2 = ${secondNumber}`)
-        } else {
-            console.log("error")
-        }
-    })
+    number.addEventListener("click", () => { 
+        if (operation === "") {
+            firstNumber += number.textContent
+            console.log(firstNumber)
+        } else if (result !== "" && operation !== "") {
+            secondNumber += number.textContent                  
+                
+        } else if (result !== "" && secondNumber === "" && operation !== "") {
+            secondNumber += number.textContent
+            console.log(secondNumber)
 
+        } else {
+            secondNumber += number.textContent
+            console.log(secondNumber)            
+        }
+        if (result !== undefined && operation === "") {
+            result += number.textContent
+        }
+
+        if (result !== undefined) {
+            display.textContent = `${result} ${operation} ${secondNumber}`
+        } else {
+            display.textContent = `${firstNumber} ${operation} ${secondNumber}`
+        }
+        
+ 
+    })
 })
 
 const equalSign = document.querySelector(".equalSign")
 
 equalSign.addEventListener("click", () => {
-    
     firstNumber = parseFloat(firstNumber)
     secondNumber = parseFloat(secondNumber)
-    showResult(operate(operation, firstNumber, secondNumber))
-    result = operate(operation, firstNumber, secondNumber)
-    firstNumber = undefined
-    secondNumber = undefined
+    if (result !== undefined) {
+        showResult(operate(operation, result, secondNumber))
+        result = operate(operation, result, secondNumber)
+
+    } else {
+        showResult(operate(operation, firstNumber, secondNumber))
+        result = operate(operation, firstNumber, secondNumber)
+    }
+   
+    firstNumber = ""
+    secondNumber = ""
+    operation = ""
     console.log(firstNumber)
     console.log(secondNumber)
 
 })
 
+const clear = document.querySelector(".clear")
+
+
+clear.addEventListener("click", () => {
+    display.textContent = ""
+})
 
 
 
